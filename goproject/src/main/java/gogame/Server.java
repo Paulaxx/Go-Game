ada230d3e92ca155bf0d14f8e6a44f199a43b4de
Paula <palinkagr@gmail.com>
2019-11-30 18:59:40 +0100

    extending the server with new functions

--- goproject/src/main/java/gogame/Server.java
+++ goproject/src/main/java/gogame/Server.java
@@ -1,26 +1,107 @@
 package gogame;
-
-import java.io.IOException;
-import java.io.PrintWriter;
-import java.net.ServerSocket;
-import java.net.Socket;
-import java.util.Scanner;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-
+import java.io.*;
+import java.net.*;
+import java.util.ArrayList;
 
 public class Server {
+		
+	static String[][] Board;
+	static int size, bot, pas=0, x, y;
+	static String actualColor="white";
 	
-	public static void main(String[] args) throws Exception {
-        try (ServerSocket listener = new ServerSocket(58901)) {
-            System.out.println("Go server is running...");
-            ExecutorService pool = Executors.newFixedThreadPool(200);
-            while (true) {
-            	pool.execute(new Player(listener.accept(), 'B'));
-            	pool.execute(new Player(listener.accept(), 'W'));
+	ServerSocket server;
+	Socket client;
+	ObjectInputStream in;
+	ObjectOutputStream out;
+	
+	
+	public Server() {
+		try {
+			server = new ServerSocket(4444); 
+		}
+		catch (IOException e) {
+			System.out.println("Could not listen on port 4444"); 
+			System.exit(-1);
+		}
+		this.listenSocket();
+	}
+	
+	
+	public void listenSocket() {
+		
+        try {
+            client = server.accept();
+        } 
+        catch (IOException e) {
+        	System.out.println("Accept failed: 4444"); 
+        	System.exit(-1);
+        }
+        
+        
+        try {
+        	
+            in = new ObjectInputStream(client.getInputStream());
+            out = new ObjectOutputStream(client.getOutputStream());
+
+            while(in != null) {
+            	@SuppressWarnings("unchecked")
+				ArrayList<String> fromSocket = (ArrayList<String>)in.readObject();
+            	if(fromSocket.get(0)=="pas") {
+            		//TO DO: co robi kiedy pasują
+            	}
+            	else if(fromSocket.get(0)=="black") {
+            		x=Integer.parseInt(fromSocket.get(1));
+            		y=Integer.parseInt(fromSocket.get(2));
+            		//TO DO: sprawdza na podstawie tablicy Board czy na danych wspolrzednych(x,y) mozna tam postawic pionka
+            		// jak mozna to wysyla do clienta 'T'  out.writeObject("T");
+            		// jak nie to wysyla 'N' out.writeObject("N");
+            	}
+            	else if(fromSocket.get(0)=="white") {
+            		x=Integer.parseInt(fromSocket.get(1));
+            		y=Integer.parseInt(fromSocket.get(2));
+            		//TO DO: sprawdza na podstawie tablicy Board czy na danych wspolrzednych(x,y) mozna tam postawic pionka
+            		// jak mozna to wysyla do clienta 'T'  out.writeObject("T");
+            		// jak nie to wysyla 'N' out.writeObject("N");
+            	}
+            	
+            	
+            	
             }
+
+        } 
+        catch(IOException | ClassNotFoundException e) {
+        	//System.out.println("Accept failed: 4444");
+            System.exit(-1);
+        }
+	
+	}
+	
+	
+	
+	protected void finalize() {
+        try {
+            in.close();
+            out.close();
+            client.close();
+            server.close();
+        } 
+        catch(IOException ex) {
+            ex.printStackTrace();
         }
     }
-
-
+	
+	
+	
+public static void main(String[] args) throws IOException{
+		ServerSocket ss=new ServerSocket(4994);
+		Socket s=ss.accept();
+		
+		InputStreamReader in = new InputStreamReader(s.getInputStream());
+		BufferedReader bf = new BufferedReader(in);
+		String properties=bf.readLine();
+		size=Integer.parseInt(properties);
+		Board = new String [size][size];
+		
+		new Server();
+	}		
 }
