daa440de35067cac7d6fcad15ed333748b12328a
Paula <palinkagr@gmail.com>
2019-11-30 23:24:01 +0100

    add gui service

--- goproject/src/main/java/gogame/Client.java
+++ goproject/src/main/java/gogame/Client.java
@@ -10,14 +10,15 @@ import java.awt.event.ActionEvent;
 
 public class Client extends JFrame implements ActionListener{
 	
+	private static final long serialVersionUID = 1L;
 	Socket Socket;
     ObjectInputStream in;
     ObjectOutputStream out;
 	
-	static String firstanswer="", size, bot;
-	JMenu move;
-	JMenuItem pas, black, white;
-	static String toSocket;
+	static String firstanswer="";
+	JMenu move, properties;
+	JMenuItem pas, black, white, size, bot;
+	static String toSocket, Size, Bot;
 	
 	public Client(){
 		setTitle("Go Game");
@@ -25,20 +26,29 @@ public class Client extends JFrame implements ActionListener{
 		
 		JMenuBar menuBar = new JMenuBar();
 		move = new JMenu("Move");
+		properties = new JMenu("properties");
 		pas = new JMenuItem("pas");
 		black = new JMenuItem("black");
 		white = new JMenuItem("white");
+		size = new JMenuItem("size");
+		bot = new JMenuItem("bot");
 		
 		setJMenuBar(menuBar);
 		menuBar.add(move);
+		menuBar.add(properties);
 		move.add(pas);
 		move.add(black);
 		move.add(white);
+		properties.add(size);
+		properties.add(bot);
 		
 		move.addActionListener(this);
+		properties.addActionListener(this);
 		pas.addActionListener(this);
 		black.addActionListener(this);
 		white.addActionListener(this);
+		size.addActionListener(this);
+		bot.addActionListener(this);
 		
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setSize(600, 600);
@@ -48,23 +58,32 @@ public class Client extends JFrame implements ActionListener{
 		
 	}
 	
-	@SuppressWarnings("unchecked")
 	public void actionPerformed(ActionEvent e) {
 		Object z = e.getSource();
 		ArrayList<String> toSocket = new ArrayList<>();
 		String x,y;
-		ArrayList<String> fromSocket =null;
 		
-		if(z==pas)
+		if(z==size) {
+			toSocket.add("size");
+			Size=JOptionPane.showInputDialog("enter size of the board");
+			toSocket.add(Size);
+		}
+		else if(z==bot) {
+			toSocket.add("bot");
+			Bot=JOptionPane.showInputDialog("Do you want to play with bot?");
+			toSocket.add(Bot);
+		}
+		
+		else if(z==pas)
 			toSocket.add("pass");
-		if(z==black) {
+		else if(z==black) {
 			toSocket.add("black");
 			x=JOptionPane.showInputDialog("enter the x coordinate:");
 			toSocket.add(x);
 			y=JOptionPane.showInputDialog("enter the y coordinate:");
 			toSocket.add(y);			
 		}
-		if(z==white) {
+		else if(z==white) {
 			toSocket.add("white");
 			x=JOptionPane.showInputDialog("enter the x coordinate:");
 			toSocket.add(x);
@@ -72,6 +91,7 @@ public class Client extends JFrame implements ActionListener{
 			toSocket.add(y);			
 		}
 		
+		System.out.println("Client send:"+toSocket);
 		try {
 			out.writeObject(toSocket);
 			out.flush();
@@ -82,16 +102,17 @@ public class Client extends JFrame implements ActionListener{
 		
 
 		try {
-	         fromSocket = (ArrayList<String>)in.readObject(); 
+			String fromSocket = (String)in.readObject(); 
+			System.out.println("Client received: "+fromSocket);
 	    } 
 		catch(Exception ex) {}
 		
-		if(fromSocket.get(0)=="T") {
+		/*if(fromSocket.get(0)=="T") {
 			//TO DO: wstawianie fromSocket.get(1) i fromSocket.get(2) (to sa wspolrzedne x i y) na nich wstawiamy pionka
 		}
 		if(fromSocket.get(0)=="N") {
 			//TO DO: wyswietl informacje zeby ktos jeszcze raz wstawil pionka
-		}
+		}*/
 	}
 	
 	
@@ -113,13 +134,6 @@ public class Client extends JFrame implements ActionListener{
 	
 	
 public static void main(String[] args) throws IOException{
-		Socket s=new Socket("localhost", 4994);
-		
-		PrintWriter out= new PrintWriter (s.getOutputStream());
-		size=JOptionPane.showInputDialog("enter size of the board");
-		bot=JOptionPane.showInputDialog("Do you want to play with bot?");
-		out.println(size);
-		out.flush();
 		
 		Client client = new Client();
 		client.setVisible(true);
